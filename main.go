7e3456a46ce51538dbfc64c312c25a2a683eabfb
Pia <pia.schliefke@awi.de>
2021-01-04 22:00:43 +0100

    changed ui toolkit (Fyne)

--- main.go
+++ main.go
@@ -1,34 +1,16 @@
-//Tutorial: https://github.com/Equanox/gotron
-
+//Tutorial: https://developer.fyne.io/tour/introduction/
 package main
 
 import (
-    "github.com/Equanox/gotron"
+	"fyne.io/fyne/app"
+	"fyne.io/fyne/widget"
 )
 
 func main() {
-    // Create a new browser window instance
-    window, err := gotron.New()
-    if err != nil {
-        panic(err)
-    }
+	app := app.New()
 
-    // Alter default window size and window title.
-    window.WindowOptions.Width = 1200
-    window.WindowOptions.Height = 980
-    window.WindowOptions.Title = "Gotron"
+	w := app.NewWindow("Hello")
+	w.SetContent(widget.NewLabel("Hello Fyne!"))
 
-    // Start the browser window.
-    // This will establish a golang <=> nodejs bridge using websockets,
-    // to control ElectronBrowserWindow with our window object.
-    done, err := window.Start()
-    if err != nil {
-        panic(err)
-    }
-    
-    // Open dev tools must be used after window.Start 
-    // window.OpenDevTools()
-    
-    // Wait for the application to close
-    <-done
+    w.ShowAndRun()
 }

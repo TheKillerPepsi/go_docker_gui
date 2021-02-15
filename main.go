b08885202534fdd2a72b12b110b01bc895b49fc6
pschlief <pschlief@awi.de>
2021-02-15 23:28:52 +0100

    started with widgets

--- main.go
+++ main.go
@@ -3,27 +3,48 @@ package main
 //Tutorial: https://developer.fyne.io/tour/basics/windows.html
 
 import (
+	//"fmt"
 	"fmt"
-	//"time"
+	"image/color"
 
-	//"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+
+	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/widget"
-	//"fyne.io/fyne/v2/canvas"
-	//"fyne.io/fyne/v2/theme"
+	//"fyne.io/fyne/v2/layout"
 )
 
 func main() {
-	main_window()
+	//mainWindow()
+	secWindows()
+
 	tidyUp()
 }
 
-func main_window() {
+func mainWindow() {
 	myApp := app.New()
-	myWindow := myApp.NewWindow("Hello")
-	myWindow.SetContent(widget.NewLabel("Hello World!"))
+	myWindow := myApp.NewWindow("Hello Canvas!")
+	//myCanvas := myWindow.Canvas()
+
+	//start with define the canvas
+	text1 := canvas.NewText("Hello World!", color.White)
+	text1.TextStyle.Italic = true
+	text2 := canvas.NewText("Hello Moon?!", color.Opaque)
+	text2.Move(fyne.NewPos(20, 20))
+	text2.TextStyle.Bold = true
+	text3 := canvas.NewText("Heyyyy Pepsi!", color.Black)
+	text3.TextStyle.Monospace = true
+	text3.Move((fyne.NewPos(40, 50)))
+
+	content := container.NewWithoutLayout(text1, text2, text3)
+	//content := container.New(layout.NewGridLayout(2), text1, text2, text3)
+	//myCanvas.SetContent(text)
+	//myWindow.SetContent(widget.NewLabel("Hello World!"))
 
 	// show and run the application
+	myWindow.SetContent(content)
 	myWindow.ShowAndRun()
 	//ShowANdRUn is short version
 	//myApp.Run()
@@ -36,3 +57,12 @@ func tidyUp() {
 
 //func first_window()
 
+func secWindows() {
+
+	myApp := app.New()
+	myWindow := myApp.NewWindow("Widget")
+
+	myWindow.SetContent(widget.NewEntry())
+	myWindow.ShowAndRun()
+
+}
